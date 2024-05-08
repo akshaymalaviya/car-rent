26fde9687ffcaaf5cb5df0eeac63e1528119fba9
Akshat Ramani <akshat.ramani@gmail.com>
2024-05-09 00:12:57 +0530

    backend development

---
+++ Front-End/recap-project/src/app/models/entities/new-car.ts
@@ -0,0 +1,12 @@
+export class NewCar {
+    carId:number;
+    brandId:number;
+    colorId:number;
+    carName:string;
+    dailyPrice:number;
+    modelYear:string;
+    description:string;
+    minFindeksScore:number;
+    image:File;
+  }
+  
